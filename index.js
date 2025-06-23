bae7fb07aa97194e4d722832dc7a6f6ad295bb8d
Logesh <tutorials.veeshatech@hotmail.com>
2025-06-23 22:19:52 +0530

    Btn Actions Updated

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 "use strict";
+
+// Current Player Index
+let curPlaInd = 1;
+
+// DOM elements
+const btnRoll = document.getElementById("roll--dice");
+const btnHold = document.getElementById("hold--score");
+const btnReset = document.getElementById("reset--game");
+const diceImage = document.getElementById("dice--image--element");
+const curPlayBg = document.querySelector(".current-player");
+
+btnRoll.addEventListener("click", function () {
+  const curScore = document.querySelector(`#player--${curPlaInd} .current`);
+  // Change the Dice Image
+  const randNum = Math.trunc(Math.random() * 6) + 1;
+  const srcUrl = `./media/dice-${randNum}.png`;
+  diceImage.setAttribute("src", srcUrl);
+  if (randNum > 1) {
+    btnHold.textContent = "HOLD SCORE";
+    // Add the Value of Dice to the Current Score
+    let val = curScore.textContent * 1;
+    val += randNum;
+    curScore.textContent = val < 10 ? "0" + val : val;
+  } else {
+    curScore.textContent = "00";
+    curPlayBg.style.left = curPlaInd == 1 ? "0vw" : "50vw";
+    curPlaInd = curPlaInd == 1 ? 2 : 1;
+  }
+});
+
+btnHold.addEventListener("click", function () {
+  btnHold.textContent = "SWITCH PLAY";
+  const totScore = document.querySelector(`#player--${curPlaInd} .total`);
+  const curScore = document.querySelector(`#player--${curPlaInd} .current`);
+  // Add the Value of Dice to the Current Score
+  let val = totScore.textContent * 1;
+  val += curScore.textContent * 1;
+  if (val > 25) {
+    alert(`Player ${curPlaInd} has won`);
+  }
+  totScore.textContent = val < 10 ? "0" + val : val;
+  curScore.textContent = "00";
+  curPlayBg.style.left = curPlaInd == 1 ? "0vw" : "50vw";
+  curPlaInd = curPlaInd == 1 ? 2 : 1;
+});
+
+btnReset.addEventListener("click", function () {
+  btnHold.textContent = "SWITCH PLAY";
+  for (let i = 1; i < 3; i++) {
+    const totScore = document.querySelector(`#player--${i} .total`);
+    const curScore = document.querySelector(`#player--${i} .current`);
+    totScore.textContent = "00";
+    curScore.textContent = "00";
+  }
+  curPlayBg.style.left = "50vw";
+  curPlaInd = 1;
+});
